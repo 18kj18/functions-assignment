8e155d936c44fa9e149fb1d2f1c400067b0d0233
18KJerzyk.ACC <18kjerzyk.acc@lwetb.ie>
2023-10-03 10:17:39 +0100

    question 119 complete

--- functions.py
+++ functions.py
@@ -1,11 +1,11 @@
 import random
 
-def question118():
+def q118():
     '''Question 118'''
     def number():
         return int(input("Enter a number --> "))
 
-    def countToNumber(x):
+    def count_to_number(x):
         count = 0
         while count <= x:
             print(count)
@@ -15,12 +15,25 @@ def question118():
             
 
 
-def question119():
+def Q119():
     '''Question 119'''
-    def randomNumber():
-        comp_num = random.randint(int(input("Enter the first number --> ")), int(input("Enter the second number --> ")))
-
-    def guessGame(num):
-        inp = int(input("Guess whant number im thingink of -->"))
+    def random_number():
+        return random.randint(int(input("Enter the first number --> ")), int(input("Enter the second number --> ")))
     
-    guessGame(randomNumber())
+    def guess_number():
+        return int(input("Guess whant number im thingink of -->"))
+    
+    def check_number(comp_num, inp):
+        if comp_num == inp:
+            print("Correct, you win!")
+        else:
+            print("Incorrect, try again")
+            self.check_number(comp_num, guess_number())
+
+    check_number(self.random_number(), guess_number())
+
+def q120():
+    '''Question 120'''
+    
+    
+    
